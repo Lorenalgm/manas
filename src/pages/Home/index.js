73b422fb62479fff7a8d8cabcb72301d07fc6442
Lorena <lorena@proesc.com>
2021-06-20 18:53:29 -0300

    Update info

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,7 +16,7 @@ export default function Home(){
             <div className="hero-container">
                 <div className="hero-content">
                     <h1>M.A.N.A.S</h1>
-                    <h3 className="subtitle">Mulheres amapaenses nas áreas de STEAM</h3>
+                    <h3 className="subtitle">Mulheres amapaenses nas áreas de STEM</h3>
                     <a className="button-talks" href="#talks" alt="Ver programação">Ver programação completa</a>
                     <div className="info-content">
                         <h3><img src={calendario} alt="Calendário" />21 a 25<br></br> de junho</h3>
